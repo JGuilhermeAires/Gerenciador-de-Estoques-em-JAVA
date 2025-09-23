e76296e390ba59ec1d2904f34c4a068204cf0226
JGuilhermeAires <jgaco@cesar.school>
2025-09-23 17:28:48 -0300

    Adicionado o update o

--- src/br/com/estoque/dao/ProdutoDAO.java
+++ src/br/com/estoque/dao/ProdutoDAO.java
@@ -2,8 +2,11 @@ package br.com.estoque.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
-import br.com.estoque.model.Produto;        
+import br.com.estoque.model.Produto;
 import br.com.estoque.factory.ConnectionFactory;
 
 public class ProdutoDAO {
@@ -24,4 +27,46 @@ public class ProdutoDAO {
             e.printStackTrace();
         }
     }
+
+    public List<Produto> getProdutos() {
+        String sql = "SELECT * FROM produtos";
+        List<Produto> produtos = new ArrayList<>();
+
+        try (Connection conn = ConnectionFactory.createConnectionToSQLServer();
+             PreparedStatement pstm = conn.prepareStatement(sql);
+             ResultSet rset = pstm.executeQuery()) {
+
+            while (rset.next()) {
+                Produto produto = new Produto();
+                produto.setId(rset.getInt("id"));
+                produto.setNome(rset.getString("nome"));
+                produto.setTipo(rset.getString("tipo"));
+                produto.setQuantidade(rset.getInt("quantidade"));
+                produto.setEstoqueMinimo(rset.getInt("estoqueMinimo"));
+
+                produtos.add(produto);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return produtos;
+    }
+    public void update(Produto produto) {
+    String sql = "UPDATE produtos SET nome = ?, tipo = ?, quantidade = ?, estoqueMinimo = ? WHERE id = ?";
+
+    try (Connection conn = ConnectionFactory.createConnectionToSQLServer();
+         PreparedStatement pstm = conn.prepareStatement(sql)) {
+
+        pstm.setString(1, produto.getNome());
+        pstm.setString(2, produto.getTipo());
+        pstm.setInt(3, produto.getQuantidade());
+        pstm.setInt(4, produto.getEstoqueMinimo());
+        pstm.setInt(5, produto.getId());
+
+        pstm.executeUpdate();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
 }
--- src/br/com/estoque/principal/Main.java
+++ src/br/com/estoque/principal/Main.java
@@ -3,18 +3,36 @@ package br.com.estoque.principal;
 import br.com.estoque.dao.ProdutoDAO;
 import br.com.estoque.model.Produto;
 
-public class Main{
-    
-    public static void main(String[] args){
-        Produto produto = new Produto();
-        produto.setNome("Caneta");
-        produto.setTipo("Escrita");
-        produto.setQuantidade(100);
-        produto.setEstoqueMinimo(10);
+public class Main {
 
+    public static void main(String[] args) {
         ProdutoDAO dao = new ProdutoDAO();
+
+        Produto produto = new Produto();
+        produto.setNome("Cadeira");
+        produto.setTipo("Objeto");
+        produto.setQuantidade(30);
+        produto.setEstoqueMinimo(12);
         dao.save(produto);
 
-        System.out.println("Produto inserido com sucesso!");
+        Produto p1 = new Produto();
+        p1.setId(1); 
+        p1.setNome("Cadeira Gamer");
+        p1.setTipo("Mobiliário");
+        p1.setQuantidade(40);
+        p1.setEstoqueMinimo(10);
+        dao.update(p1);
+
+        System.out.println("Produto inserido/atualizado com sucesso!\n");
+
+        for (Produto p : dao.getProdutos()) {
+            System.out.println(
+                "ID: " + p.getId() +
+                " | Nome: " + p.getNome() +
+                " | Tipo: " + p.getTipo() +
+                " | Quantidade: " + p.getQuantidade() +
+                " | Estoque Mínimo: " + p.getEstoqueMinimo()
+            );
+        }
     }
 }
